6df2048b5a4efe0c25132586bab5860e191386b5
Landysh Khusaenova <landysh.kg@gmail.com>
2023-10-30 20:08:49 +0100

    added file

---
+++ post.js
@@ -0,0 +1,19 @@
+let packageInfo = {
+  id: "1432HGF",
+  codeNumber: "A001DFX0",
+  cellNumber: 4,
+  cost: 100,
+};
+
+let testCodeNumber = "A001DFX0";
+let pochtamat = [null, null, null, "1432HGF", null];
+let wallet = 500;
+
+if ((packageInfo.codeNumber === testCodeNumber)) {
+  newBalance = wallet - packageInfo.cost;
+  console.log(
+    `Заберите посылку ${packageInfo.id} из ячейки №${packageInfo.cellNumber}. Ваш счет составляет: ${newBalance}ед.`
+  );
+} else {
+  console.log("Wrong code");
+}
